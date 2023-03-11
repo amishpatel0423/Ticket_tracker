a063f1fb7f84db63e8f60af29e89d57ae1da4624
Amishwho <amishpatel0423@gmail.com>
2023-03-10 22:31:56 -0600

    Finally fixed the css issues by creating a folder

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ const app = express();
 const port = 3000;
 
 
-
-app.use(express.static(__dirname+  '/Signin'));
+app.use(express.static(__dirname + '/public/'));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname+ '/index.html');

ac0085f8724d384debc1fca190de2665e9c36364
Amishwho <amishpatel0423@gmail.com>
2023-03-11 10:05:12 -0600

    Removed unnesscary folders signin folder as public

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const port = 3000;
 
 
 app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/Signin/'));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname+ '/index.html');

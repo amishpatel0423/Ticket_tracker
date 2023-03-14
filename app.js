8c6d5776675745dcd18a509e5c5f02abf0389ff0
NicholasBolen <nickbyellow@gmail.com>
2023-03-13 20:48:02 -0600

    Handle account creation, first bacckend request
    Co-authored-by: amishpatel0423@gmail.com <amishpatel0423@users.noreply.github.com>

--- app.js
+++ app.js
@@ -3,66 +3,70 @@ import {} from 'dotenv/config';
 import mongoose from 'mongoose';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { Department, User } from './schemas/schemas.js';
+import express from 'express';
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-import { Department } from './schemas/schemas.js';
-
-var msg = 'Hello World!';
-console.log(msg);
 
 const uri = `mongodb+srv://user0:${process.env.MONGO_KEY}@cluster0.tpyq1gp.mongodb.net/?retryWrites=true&w=majority`;
 
-import express from 'express';
 const app = express();
 const port = 3000;
 
 
+// Public HTML/files
 app.use(express.static(__dirname + '/public/'));
 app.use(express.static(__dirname + '/Signin/'));
+app.use(express.urlencoded({ extended: true }));
 
+// HTML get
 app.get('/', (req, res) => {
 	res.sendFile(__dirname+ '/index.html');
 });
-
 app.get('/login',(req,res)=>{
 	res.sendFile(__dirname+ '/Signin/signin.html');
 });
-
 app.get('/create',(req,res)=>{
 	res.sendFile(__dirname+ '/Signin/createticket.html');
 });
-
 app.get('/signup',(req,res)=>{
 	res.sendFile(__dirname+ '/Signin/Signup.html');
 });
-
 app.get('/dashboard',(req,res)=>{
 	res.sendFile(__dirname+ '/Signin/dashboard.html');
 });
-
 app.get('/profile',(req,res)=>{
 	res.sendFile(__dirname+ '/Signin/profile.html');
 });
 
+// Post requests
+app.post('/signup', (req, res)=>{
+	console.log(req.body);
+	var pass, uname, email = req.body;
+
+	create_account(uname, pass, email).catch(err => console.log(err));
+	res.sendFile(__dirname+ '/Signin/dashboard.html');
+});
 
 
-main().catch(err => console.log(err));
-
-async function main() {
+async function create_account(fname, pass, email) {
 	await mongoose.connect(uri);
 
-	/*
-	const TechSupport = new Department({
-		id: 0,
-		name: 'Tech Support',
+	const TechSupport = new User({
+		id: email,
+		email: email,
+		password: pass,
+		username: fname,
+		avatar: '',
+		permission_level: 'User'
 	});
 	await TechSupport.save();
-	*/
 	
 	const departments = await Department.find();
 	console.log(departments);
 }
 
 app.listen(port);
+console.log(`http://localhost:${port}`);
